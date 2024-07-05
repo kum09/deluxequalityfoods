40bfdd072fc319ca0874e9e904c8dd08e2ee551d
Anil <kum09a@gmail.com>
2024-07-05 22:29:28 +0530

    update data

---
+++ public/frontend/assets/include/footer.php
@@ -0,0 +1,135 @@
+<footer class="ps-footer">
+        <div class="container">
+          <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 ">
+                  <div class="ps-widget ps-widget--footer ps-widget--payment">
+                    <div class="ps-widget__header">
+                      <h3 class="ps-widget__title">PAYMENTS ACCEPTED</h3>
+                    </div>
+                    <div class="ps-widget__content">
+                      <ul>
+                        <li><a href="#"><img src="images/payment/1.png" alt=""></a></li>
+                        <li><a href="#"><img src="images/payment/2.png" alt=""></a></li>
+                        <li><a href="#"><img src="images/payment/3.png" alt=""></a></li>
+                        <li><a href="#"><img src="images/payment/4.png" alt=""></a></li>
+                        <li><a href="#"><img src="images/payment/5.png" alt=""></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 ">
+                  <div class="ps-widget ps-widget--footer ps-widget--worktime">
+                    <div class="ps-widget__header">
+                      <h3 class="ps-widget__title">WORK TIME</h3>
+                    </div>
+                    <div class="ps-widget__content">
+                      <p><strong>Monday - Friday</strong> 8:00 am - 8:30 pm</p>
+                      <p><strong>Satuday - Sunday</strong>10:00 am - 16:30 pm</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 ">
+                  <div class="ps-widget ps-widget--footer ps-widget--order">
+                    <div class="ps-widget__header">
+                      <h3 class="ps-widget__title">Quick Links</h3>
+                    </div>
+                    <div class="ps-widget__content">
+                          <ul class="ps-list--line">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Product</a></li>
+                            <li><a href="#">Blog</a></li>
+                          </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 ">
+                  <div class="ps-widget ps-widget--footer ps-widget--connect">
+                    <div class="ps-widget__header">
+                      <h3 class="ps-widget__title">CONNECT US</h3>
+                    </div>
+                    <div class="ps-widget__content">
+                      <p>Address: 0wilkinson road , Unit#27&28, L6T5B1, Brampton</p>
+                      <p>Mail: abc@gmail.com</p>
+                      <p>Phone: +1(437)-870-8549</p> 
+                          <ul class="ps-widget__social">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                          </ul>
+                          <!-- <a href="#"><img src="images/app.jpg" alt=""></a> -->
+                      <p><a href="#"> Deluxe</a> © 2024 All Right Reserved.</p>
+                    </div>
+                  </div>
+                </div>
+          </div>
+        </div>
+      </footer>
+      <div class="modal-popup mfp-with-anim mfp-hide" id="quickview-modal" tabindex="-1">
+        <button class="modal-close"><i class="fa fa-remove"></i></button>
+        <div class="ps-product-modal ps-product--detail clearfix">
+              <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 ">
+                <div class="ps-product__thumbnail">
+                  <div class="quickview--main" data-owl-auto="true" data-owl-loop="false" data-owl-speed="10000" data-owl-gap="0" data-owl-nav="false" data-owl-dots="false" data-owl-animate-in="" data-owl-animate-out="" data-owl-item="1" data-owl-item-xs="1" data-owl-item-sm="1" data-owl-item-md="1" data-owl-item-lg="1" data-owl-nav-left="&lt;i class=&quot;fa fa-angle-left&quot;&gt;&lt;/i&gt;" data-owl-nav-right="&lt;i class=&quot;fa fa-angle-right&quot;&gt;&lt;/i&gt;">
+                    <div class="ps-product__image"><img src="images/cake/img-cake-12.jpg" alt=""></div>
+                    <div class="ps-product__image"><img src="images/cake/img-cake-11.jpg" alt=""></div>
+                    <div class="ps-product__image"><img src="images/cake/img-cake-10.jpg" alt=""></div>
+                    <div class="ps-product__image"><img src="images/cake/img-cake-6.jpg" alt=""></div>
+                    <div class="ps-product__image"><img src="images/cake/img-cake-5.jpg" alt=""></div>
+                  </div>
+                  <div class="quickview--thumbnail" data-owl-auto="true" data-owl-loop="false" data-owl-speed="10000" data-owl-gap="20" data-owl-nav="false" data-owl-dots="false" data-owl-animate-in="" data-owl-animate-out="" data-owl-item="4" data-owl-item-xs="2" data-owl-item-sm="3" data-owl-item-md="4" data-owl-item-lg="4" data-owl-nav-left="&lt;i class=&quot;fa fa-angle-left&quot;&gt;&lt;/i&gt;" data-owl-nav-right="&lt;i class=&quot;fa fa-angle-right&quot;&gt;&lt;/i&gt;"><img src="images/cake/img-cake-12.jpg" alt=""><img src="images/cake/img-cake-11.jpg" alt=""><img src="images/cake/img-cake-10.jpg" alt=""><img src="images/cake/img-cake-6.jpg" alt=""><img src="images/cake/img-cake-5.jpg" alt=""></div>
+                </div>
+              </div>
+              <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 ">
+                <header>
+                  <h3 class="ps-product__name">Anytime Cakes</h3>
+                  <select class="ps-rating">
+                    <option value="1">1</option>
+                    <option value="1">2</option>
+                    <option value="1">3</option>
+                    <option value="1">4</option>
+                    <option value="5">5</option>
+                  </select>
+                  <p class="ps-product__price">£15.00 <del>£25.00</del></p>
+                  <div class="ps-product__meta">
+                    <p><span> Availability: </span> In stock</p>
+                    <p class="category"><span>CATEGORIES: </span><a href="product-grid.html">Cupcake</a>,<a href="product-grid.html">organic</a>,<a href="product-grid.html"> sugar</a>,<a href="product-grid.html"> sweet</a>,<a href="product-grid.html"> bio</a></p>
+                  </div>
+                  <div class="form-group ps-product__size">
+                    <label>Size:</label>
+                    <select class="ps-select" data-placeholder="Popupar product">
+                      <option value="01">Choose a option</option>
+                      <option value="01">Item 01</option>
+                      <option value="02">Item 02</option>
+                      <option value="03">Item 03</option>
+                    </select>
+                  </div>
+                  <div class="ps-product__shop">
+                    <div class="form-group--number">
+                      <button class="minus"><span>-</span></button>
+                      <input class="form-control" type="text" value="1">
+                      <button class="plus"><span>+</span></button>
+                    </div>
+                    <ul class="ps-product__action">
+                      <li><a href="#" data-tooltip="Add to wishlist"><i class="ps-icon--heart"></i></a></li>
+                      <li><a href="#" data-tooltip="Compare"><i class="ps-icon--reload"></i></a></li>
+                    </ul>
+                  </div>
+                </header>
+                <footer><a class="ps-btn--fullwidth ps-btn ps-btn--sm" href="#">Purchase<i class="fa fa-angle-right"></i></a>
+                  <p class="ps-product__sharing">Share with:<a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-google-plus"></i></a><a href="#"><i class="fa fa-twitter"></i></a></p>
+                </footer>
+              </div>
+        </div>
+      </div>
+      <div class="mfp-with-anim modal-popup mfp-hide" id="modal--subscribe">
+        <button class="modal-close"><i class="fa fa-remove"></i></button><img src="images/img-demo-4.png" alt="">
+        <form action="https://nouthemes.net/html/bakery/_action" method="post">
+          <h3>STAY UP-TO-DATE  WITH OUR NEWLETTER</h3>
+          <p>Follow us & get <span> 20% OFF </span> coupon for first purchase !!!!!</p>
+          <div class="form-group">
+            <input class="form-control" type="text" placeholder="Type your email...">
+            <button class="ps-btn ps-btn--sm">Subscribe</button>
+          </div>
+        </form>
+      </div>
---
+++ resources/views/layouts/frontend/header.blade.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link href="apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="images/logo.png" rel="icon">
+    <meta name="author" content="#{author}">
+    <meta name="keywords" content="#{keyword}">
+    <meta name="description" content="#{description}">
+    <title>Deluxe</title>
+    <!-- Fonts-->
+
+    <!-- <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700%7CPacifico%7CVarela+Round%7CPoppins" rel="stylesheet"> -->
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/jquery-bar-rating/dist/themes/fontawesome-stars.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/ps-icon/ps-icon.css')}}">
+    
+    <!-- CSS Library-->
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/bootstrap/dist/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/owl-carousel/assets/owl.carousel.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/jquery-bar-rating/dist/themes/fontawesome-stars.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/Magnific-Popup/dist/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/jquery-ui/jquery-ui.min.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/slick/slick/slick.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/lightGallery-master/dist/css/lightgallery.min.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/revolution/css/settings.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/revolution/css/layers.css')}}">
+    <link rel="stylesheet" href="{{url('public/frontend/assets/plugins/revolution/css/navigation.css')}}">
+    <!-- Custom-->
+    <link rel="stylesheet" href="{{url('public/frontend/assets/css/style.css')}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link href="apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{url('public/frontend/assets/images/logo.png')}}" rel="icon">
+    <meta name="author" content="#{author}">
+    <meta name="keywords" content="#{keyword}">
+    <meta name="description" content="#{description}">
+  </head> 
+  <body class="page-init">
+    
+	<div class="ps-searchbox">
+      <div class="ps-searchbox__remove"><i class="fa fa-remove"></i></div>
+      <div class="container">
+        <header>
+          <p>Enter your keywords:</p>
+          <form method="post" action="https://nouthemes.net/product-grid.php">
+            <input class="form-control" type="text" placeholder="">
+            <button><i class="ps-icon--search"></i></button>
+          </form>
+        </header>
+        <div class="ps-searchbox__result">
+          <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                  <div class="ps-product--list ps-product--list-light mt-60">
+                    <div class="ps-product__thumbnail"><a class="ps-product__overlay" href="#"></a><img src="images/products/1.png" alt=""></div>
+                    <div class="ps-product__content">
+                      <h4 class="ps-product__title"><a href="#">MARBLE POUND CAKE</a></h4>
+                      <p>A Classic Treat With A Beautiful Swirl Of Vanilla And Chocolate Flavors.</p>
+                      <p class="ps-product__price">
+                        <del>₹25.00</del>₹15.00
+                      </p><a class="ps-btn ps-btn--xs" href="cart.php">Order now<i class="fa fa-angle-right"></i></a>
+                    </div>
+                  </div>
+                  <div class="ps-product--list ps-product--list-light mt-60">
+                    <div class="ps-product__thumbnail"><a class="ps-product__overlay" href="#"></a><img src="images/cake/img-cr-2.jpg" alt=""></div>
+                    <div class="ps-product__content">
+                      <h4 class="ps-product__title"><a href="#">MILK PUNJABI BISCUIT</a></h4>
+                      <p>Traditional Punjabi Biscuits Are Made With Rich Milk For A Delightful Taste.</p>
+                      <p class="ps-product__price">
+                        <del>₹25.00</del>₹15.00
+                      </p><a class="ps-btn ps-btn--xs" href="cart.php">Order now<i class="fa fa-angle-right"></i></a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                  <div class="ps-product--list ps-product--list-light mt-60">
+                    <div class="ps-product__thumbnail"><a class="ps-product__overlay" href="#"></a><img src="images/cake/img-cr-3.jpg" alt=""></div>
+                    <div class="ps-product__content">
+                      <h4 class="ps-product__title"><a href="#">VANILLA POUND CAKE</a></h4>
+                      <p>A Rich, Buttery Pound Cake Infused With The Warm Flavor Of Vanilla.</p>
+                      <p class="ps-product__price">
+                        <del>₹25.00</del>₹15.00
+                      </p><a class="ps-btn ps-btn--xs" href="cart.php">Order now<i class="fa fa-angle-right"></i></a>
+                    </div>
+                  </div>
+                  <div class="ps-product--list ps-product--list-light mt-60">
+                    <div class="ps-product__thumbnail"><a class="ps-product__overlay" href="#"></a><img src="images/cake/img-cr-4.jpg" alt=""></div>
+                    <div class="ps-product__content">
+                      <h4 class="ps-product__title"><a href="#">The Crusty Croissant</a></h4>
+                      <p>Lollipop dessert donut marzipan cookie bonbon sesame snaps chocolate.</p>
+                      <p class="ps-product__price">
+                        <del>₹25.00</del>₹15.00
+                      </p><a class="ps-btn ps-btn--xs" href="cart.php">Order now<i class="fa fa-angle-right"></i></a>
+                    </div>
+                  </div>
+                </div>
+          </div>
+        </div>
+        <footer class="text-center"><a class="ps-searchbox__morelink" href="product-grid.php">VIEW ALL RESULT</a></footer>
+      </div>
+    </div>
+    <div class="header--sidebar"></div>
+    <header class="header" data-responsive="1199">
+      <div class="header__top">
+        <div class="container">
+          <div class="row">
+                <div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 ">
+                  <p>10wilkinson road , Unit#27&28, L6T5B1, Brampton:  </p>
+                </div>
+                <div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 ">
+                <p class="text-right" >+1(437)-870-8549</p>
+                  <!-- <a class="ps-search-btn" href="#"><i class="ps-icon--search"></i></a> -->
+                  <!-- <div class="btn-group ps-dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">USD<i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">USD</a></li>
+                      <li><a href="#">SGD</a></li>
+                      <li><a href="#">ERO</a></li>
+                      <li><a href="#">JPN</a></li>
+                    </ul>
+                  </div> -->
+                  <!-- <div class="btn-group ps-dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Language<i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">English</a></li>
+                      <li><a href="#">Japanese</a></li>
+                      <li><a href="#">Chinese</a></li>
+                    </ul>
+                  </div> -->
+                </div>
+          </div>
+        </div>
+      </div>
+      <nav class="navigation">
+    <div class="container">
+        <div class="menu-toggle"><span></span></div>
+        <div class="navigation__left">
+            <ul class="menu menu--left" id="left-menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="products.php">Products</a></li>
+            </ul>
+        </div>
+        <a class="ps-logo" href="index.php"><img src="{{url('public/frontend/assets/images/logo.png')}}" alt=""></a>
+        <div class="navigation__right">
+            <ul class="menu menu--right" id="right-menu">
+                <!-- <li><a href="#">Gallery</a></li> -->
+                <!-- <li><a href="blog-listing.php">Blogs</a></li> -->
+                <li><a href="contact.php">Contact Us</a></li>
+                <li><a href="checkout.php">Checkout</a></li>
+                <li><a href="cart.php">Cart</a></li>
+            </ul>
+            <div class="ps-cart"><a class="ps-cart__toggle" href="#"><span><i>20</i></span><i class="ps-icon--shopping-cart"></i></a>
+                <div class="ps-cart__listing">
+                    <div class="ps-cart__content">
+                        <div class="ps-cart-item"><a class="ps-cart-item__close" href="#"></a>
+                            <div class="ps-cart-item__thumbnail"><img src="images/products/1.png" alt=""></div>
+                            <div class="ps-cart-item__content"><a class="ps-cart-item__title" href="cart.php">MARBLE POUND CAKE</a>
+                                <p><span>Quantity:<i>12</i></span><span>Total:<i>₹176</i></span></p>
+                            </div>
+                        </div>
+                        <div class="ps-cart-item"><a class="ps-cart-item__close" href="#"></a>
+                            <div class="ps-cart-item__thumbnail"><img src="images/products/2.png" alt=""></div>
+                            <div class="ps-cart-item__content"><a class="ps-cart-item__title" href="cart.php">VANILLA POUND CAKE</a>
+                                <p><span>Quantity:<i>12</i></span><span>Total:<i>₹176</i></span></p>
+                            </div>
+                        </div>
+                        <div class="ps-cart-item"><a class="ps-cart-item__close" href="#"></a>
+                            <div class="ps-cart-item__thumbnail"><img src="images/products/8.png" alt=""></div>
+                            <div class="ps-cart-item__content"><a class="ps-cart-item__title" href="cart.php">MILK PUNJABI BISCUIT</a>
+                                <p><span>Quantity:<i>12</i></span><span>Total:<i>₹176</i></span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ps-cart__total">
+                        <p>Number of items:<span>36</span></p>
+                        <p>Item Total:<span>₹528.00</span></p>
+                    </div>
+                    <div class="ps-cart__footer"><a class="ps-btn ps-btn--view-bag" href="cart.php">View bag</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+
+    </header>
+    <div id="back2top"><i class="fa fa-angle-up"></i></div>
+    <div class="loader"></div>
